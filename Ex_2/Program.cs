1f86e313ce791884a640284dd7577b7b6dea5c31
Yuri <butor.yuri@gmail.com>
2022-12-21 11:34:35 +0300

    add second exercise

---
+++ Ex_2/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+
+
+try
+{
+    int[,] array = Create2DArray(3,4);
+    Print2DArray(array);
+
+    int m = ReadInt("Введите первое число: ");
+    int n = ReadInt("Введите второе число: ");
+
+    FindArrayNumber(m, n, array);
+   
+}
+
+
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+void FindArrayNumber (int a, int b, int[,] array)
+{
+    if (a > (array.GetLength(0)-1) || b > array.GetLength(1)-1) Console.WriteLine ("Такого числа в массиве нет");
+    else Console.WriteLine (array [a,b]);
+}
+
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray(int m, int n)
+{
+    int[,] array = new int[m,n];
+
+    Random random = new Random();
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = random.Next(1,100);
+        }
+    }
+
+    return array;
+}
+
+int ReadInt(string title)
+{
+    Console.WriteLine(title);
+
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParsed)
+    {
+        return number;
+    }
+    
+    throw new Exception("Введены не корректные символы");
+}
